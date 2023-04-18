a20bd0494bf177f29b9bab72d299186cfa7f4d78
Padmaj Verma <65193136+sasukexclan@users.noreply.github.com>
2023-04-19 00:59:49 +0530

    Add files via upload

---
+++ Keeper/src/component/App.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import Header from "./Header.jsx"
+import Footer from "./Footer.jsx"
+import Note from "./Note.jsx"
+
+function App() {
+  return (
+    //<h1>YO</h1>
+    <div>
+
+        <Header />
+        <Note />
+        <Footer />
+       
+
+    </div>
+  )
+}
+
+export default App;
---
+++ Keeper/src/component/Note.jsx
@@ -0,0 +1,14 @@
+import React from "react"; 
+
+function Note() {
+  return (
+    <div className="note">
+
+       <h1>This is the title</h1>
+       <p>This is the main paragraph</p>
+    
+    </div>
+  )
+}
+
+export default Note;
